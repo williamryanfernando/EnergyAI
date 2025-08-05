2d10be10894c7ce14d05ff6b6f9abb8ee7009f40
williamryanfernando <156137397+williamryanfernando@users.noreply.github.com>
2025-08-05 14:15:41 -0400

    update
    completed the code for the training

--- model_train.py
+++ model_train.py
@@ -9,7 +9,6 @@ from transformers import (
 )
 from trl import SFTTrainer
 
-# 1. Configuration
 model_name = "microsoft/phi-3-mini-4k-instruct"
 dataset_name = "training_dataset.json"  
 new_model_name = "unit-ai-phi3-mini"  
@@ -41,3 +40,48 @@ lora_config = LoraConfig(
     task_type="CAUSAL_LM",
     target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"],
 )
+
+model = get_peft_model(model, lora_config)
+
+def format_instruction(sample):
+	return f"<|user|>\n{sample['instruction']}<|end|>\n<|assistant|>\n{sample['output']}<|end|>"
+
+dataset = load_dataset("json", data_files=dataset_name, split="train")
+
+training_arguments = TrainingArguments(
+    output_dir="./results",
+    num_train_epochs=3,     
+    per_device_train_batch_size=1, 
+    gradient_accumulation_steps=4, 
+    optim="paged_adamw_32bit",
+    save_steps=50,         
+    logging_steps=10,       
+    learning_rate=2e-4,
+    weight_decay=0.001,
+    fp16=False,
+    bf16=True,             
+    max_grad_norm=0.3,
+    max_steps=-1,
+    warmup_ratio=0.03,
+    group_by_length=True,
+    lr_scheduler_type="constant",
+)
+
+trainer = SFTTrainer(
+    model=model,
+    train_dataset=dataset,
+    peft_config=lora_config,
+    dataset_text_field="text", 
+    max_seq_length=1024,      
+    tokenizer=tokenizer,
+    args=training_arguments,
+    formatting_func=format_instruction, 
+)
+
+print("Starting model fine-tuning...")
+trainer.train()
+print("Fine-tuning complete.")
+
+print(f"Saving fine-tuned model to ./{new_model_name}")
+trainer.model.save_pretrained(new_model_name)
+print("Model saved successfully.")
